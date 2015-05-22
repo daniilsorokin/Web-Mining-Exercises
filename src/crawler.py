e5aacb202e79aca4675473c9dabb34559ca86d26
daniilsorokin <sorokin@ukp.informatik.tu-darmstadt.de>
2015-05-22 18:21:58 +0200

    server lock and statistics for extracted urls

--- src/crawler.py
+++ src/crawler.py
@@ -5,34 +5,40 @@ Created on May 20, 2015
 '''
 
 from urllib.request import urlopen
-from urllib.parse import urljoin,urldefrag
+from urllib.parse import urljoin,urldefrag, urlparse
 
 from bs4 import BeautifulSoup
 
 class Crawler:
     def __init__(self, u_limit):
         self._queue = set()
-        self._urls_visited = 0
+        self._servers = {}
+        self._extracted = {}
+        self._no_urls_visited = 0
         self._limit = u_limit
+        
     
     def run(self):
-        while self._urls_visited < self._limit and len(self._queue) > 0:
+        while self._no_urls_visited < self._limit and len(self._queue) > 0:
             current_url = self._queue.pop()
             soup = self._openUrl(current_url)
-            self._urls_visited += 1
+            self._no_urls_visited += 1
             urls = self._get_urls_from_soup(soup)
             urls = self._canonize_links(current_url, urls)
             self.add_to_queue(urls)
             self._log(current_url, urls)
         
     def add_to_queue(self, urls):
+#         for server in [urlparse(url).netloc for url in urls]:
+#             if server not in self._servers:
+#                 self._servers[server] = None;
         self._queue.update(urls)
 
     def get_statistics(self):
         return None;
     
     def _log(self, visited_url, extracted_urls):
-        print("{}: {} -> {}".format(self._urls_visited, visited_url, len(extracted_urls)))
+        print("{}: {} -> {}".format(self._no_urls_visited, visited_url, len(extracted_urls)))
         
     def _openUrl(self, url):
         with urlopen(url) as d:
@@ -40,10 +46,10 @@ class Crawler:
         return soup
 
     def _get_urls_from_soup(self, soup):
-        return {url.get('href') for url in soup.find_all('a') if url.has_attr('href')}
+        return [url.get('href') for url in soup.find_all('a') if url.has_attr('href')]
 
     def _canonize_links(self, base, urls):
-        return {urljoin(base, urldefrag(url)[0]) for url in urls}
+        return [urljoin(base, urldefrag(url)[0]) for url in urls]
     
 if __name__ == '__main__':
     crawler = Crawler(10)
